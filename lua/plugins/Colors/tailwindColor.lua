4d56d32c442f79b342beab76cd2831b9217e1215
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2024-12-15 18:53:16 +0530

    Added Link-opner with wslview
    install wslview with "sudo nala install wslu"
    now on any link <gx> will open it inside browser.

--- lua/plugins/Colors/tailwindColor.lua
+++ lua/plugins/Colors/tailwindColor.lua
@@ -27,18 +27,38 @@ return {
     },
   },
   -- TODO:
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   dependencies = {
-  --     { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
-  --   },
-  --   opts = function(_, opts)
-  --     -- original LazyVim kind icon formatter
-  --     local format_kinds = opts.formatting.format
-  --     opts.formatting.format = function(entry, item)
-  --       format_kinds(entry, item) -- add icons
-  --       return require("tailwindcss-colorizer-cmp").formatter(entry, item)
-  --     end
-  --   end,
-  -- },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+    },
+    opts = function(_, opts)
+      -- original LazyVim kind icon formatter
+      local format_kinds = opts.formatting.format
+      opts.formatting.format = function(entry, item)
+        format_kinds(entry, item) -- add icons
+        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+      end
+    end,
+  },
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig", -- optional
+    },
+    opts = {}, -- your configuration
+  },
+  {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    -- optionally, override the default options:
+    config = function()
+      require("tailwindcss-colorizer-cmp").setup({
+        color_square_width = 2,
+      })
+    end,
+  },
 }
