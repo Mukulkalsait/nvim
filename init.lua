f8727857c93ce16d79513a8d7a4a2a4eb47b8fde
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-02-19 22:05:44 +0530

    latest commit Mukul

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.cmd("colorscheme tokyonight-night") -- Replace 'tokyonight' with your desired theme
+-- vim.cmd("colorscheme tokyonight-night") -- Replace 'tokyonight' with your desired theme
+vim.cmd("colorscheme cyberdream") -- Replace 'tokyonight' with your desired theme
 vim.o.signcolumn = "yes" -- Show sign column for diagnostics
 vim.o.updatetime = 250 -- Update delay for diagnostics
