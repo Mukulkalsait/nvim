2ba479c1e54abcdf671313c827daf48fa1401e65
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-02-19 21:42:34 +0530

    config seperations

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,8 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "plugins/Lsp" },
+    { import = "plugins/Themes" },
     { import = "plugins/Colors" },
   },
   defaults = {
