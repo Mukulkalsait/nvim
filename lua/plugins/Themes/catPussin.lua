c756787bbac947a8dfd49cad1a3d587e305a8865
mukulkalsait <mdk.121.72633.sg@gamil.com>
2025-06-20 23:57:32 +0530

    catpussin transperancy changed to true

--- lua/plugins/Themes/catPussin.lua
+++ lua/plugins/Themes/catPussin.lua
@@ -4,7 +4,7 @@ return {
     name = "catppuccin",
     opts = {
       term_colors = true,
-      transparent_background = false,
+      transparent_background = true,
       styles = {
         comments = {},
         conditionals = {},
