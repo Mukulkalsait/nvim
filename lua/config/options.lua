fc647f0697568da0027fa39c132658b7b1ba6c59
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-09-22 13:30:53 +0530

    update on nix

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,11 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.shell = "/usr/bin/zsh"
+vim.opt.shell = "/etc/profiles/per-user/mukuldk/bin/zsh" -- Y:  the correct path from `which zsh`
 
 -- Configure LazyVim's built-in terminal
 vim.g.terminal_float_type = "snacks"
 vim.g.terminal_position = "float" -- This should make it float
-
 -- Additional terminal settings
-vim.g.terminal_shell = "/usr/bin/zsh"
+-- vim.g.terminal_shell = "/usr/bin/zsh"
