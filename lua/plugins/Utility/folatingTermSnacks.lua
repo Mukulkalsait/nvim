a405f964f888ea317045c5ca5d33dff1e1d09032
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-09-22 13:30:59 +0530

    update on nix

--- lua/plugins/Utility/folatingTermSnacks.lua
+++ lua/plugins/Utility/folatingTermSnacks.lua
@@ -28,6 +28,7 @@ return {
     "folke/snacks.nvim",
     opts = {
       terminal = {
+        shell = vim.o.shell, -- Y: or /etc/profiles/per-user/mukuldk/bin/zsh
         win = {
           position = "float",
         },
