2565db30a94226223ad500c16f36310fe05372ee
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-09-22 13:31:04 +0530

    update on nix

--- init.lua
+++ init.lua
@@ -1,11 +1,15 @@
+--
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
 -- vim.cmd("colorscheme tokyonight-night") -- Replace 'tokyonight' with your desired theme
-vim.cmd("colorscheme cyberdream") -- Replace 'tokyonight' with your desired theme
+vim.cmd("colorscheme tokyonight-night") -- Replace 'tokyonight' with your desired theme
+-- vim.cmd("colorscheme tokyonight-moon") -- Replace 'tokyonight' with your desired theme
+
 vim.o.signcolumn = "yes" -- Show sign column for diagnostics
 vim.o.updatetime = 250 -- Update delay for diagnostics
+vim.opt.clipboard = "unnamedplus"
 -- Load performance optimizations
 require("config.performance")
 
-vim.filetype.add({ extension = { rgvp = "php" } })
+-- vim.filetype.add({ extension = { rgvp = "php" } })
