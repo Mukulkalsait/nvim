4f471f3656a9481b990e83a0429e9580dacdab5d
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-10-25 23:27:32 +0530

    and all other with nix

--- init.lua
+++ init.lua
@@ -13,3 +13,42 @@ vim.opt.clipboard = "unnamedplus"
 require("config.performance")
 
 -- vim.filetype.add({ extension = { rgvp = "php" } })
+require'lspconfig'.rust_analyzer.setup {
+
+  on_attach = function(client, bufnr)
+    -- 🧠 Enable inlay hints automatically when LSP attaches
+    if client.server_capabilities.inlayHintProvider then
+      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+    end
+  --   -- (Optional) Toggle keybinding: <leader>th
+  --   vim.keymap.set("n", "<leader>th", function()
+  --     local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
+  --     vim.lsp.inlay_hint.enable(not enabled, { bufnr = bufnr })
+  --   end, { buffer = bufnr, desc = "Toggle Inlay Hints" })
+  end,
+
+  settings = {
+    ['rust-analyzer'] = {
+      checkOnSave = { command = "clippy" },
+      rust = {
+        analyzerTargetDir = true,
+        linkedProjects = { "Cargo.toml" },
+      },
+      cargo = {
+        allFeatures = true,
+        buildScripts = { enable = true },
+        loadOutDirsFromCheck = true,
+      },
+      diagnostics = { enable = true },
+      files = {
+        excludeDirs = { ".direnv", ".git", ".github", ".gitlab", "bin", "node_modules", "target", "venv", ".venv" },
+      },
+      procMacro = { enable = true },
+    },
+  },
+  env = {
+    RUST_SRC_PATH = vim.env.RUST_SRC_PATH,  -- Explicitly pass RUST_SRC_PATH
+  },
+}
+
+
