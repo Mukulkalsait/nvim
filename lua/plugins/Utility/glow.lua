1780cd0a0274a004d80a70808a75ee1d341f1e15
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-10-27 16:19:06 +0530

    Updates

--- lua/plugins/Utility/glow.lua
+++ lua/plugins/Utility/glow.lua
@@ -1,29 +1,27 @@
 
 return {
   "ellisonleao/glow.nvim",
-  config = true,
+  -- config = true,
   cmd = "Glow",
   keys = {
     { "<leader>mp", "<cmd>Glow<cr>", desc = " Markdown Preview (Glow)" },
   },
 
   opts = {
-    -- style = "dracula", -- "auto" tries to detect your Neovim background option. Possible values: "dark", "light", "auto"
-    style = "~/.config/nvim/plugins/Utility/glowDraculaCostume.json", -- "auto" tries to detect your Neovim background option. Possible values: "dark", "light", "auto"
-    pager= false, -- with paper true, I Have not idea what difference it make.
-    width = 500,
-    height = 500,
+    -- style = "dark", -- "auto" tries to detect your Neovim background option. Possible values: "dark", "light", "auto"
+    -- style = "~/.config/nvim/plugins/Utility/glowDraculaCostume.json", -- "auto" tries to detect your Neovim background option. Possible values: "dark", "light", "auto"
+    -- pager= false, -- with paper true, I Have not idea what difference it make.
     width_ratio = 0.85,
     height_ratio = 0.75,
     border = "rounded", -- "none", "single", "double", "rounded", "solid", "shadow"
-    position = "right", -- Window position (center, top, bottom, left, right) only in flowting mod
-    padding = 0, -- Padding between window border and content
-    log_level = "error", -- values: "error", "warn", "info", "debug" Enable or disable Glow's command output logging (for debugging)
-    auto_close = true, -- Automatically close Glow preview when you leave the buffer
-    float = false, -- Open Glow in a floating window (true) or full Neovim buffer (false) Set this to false if you want tab-based or split-based previews
-    -- glow_path = nil, -- Custom path to your `glow` binary if it's not in $PATH
-    syntax = true, -- Enable syntax highlighting within the Glow preview (requires glow ≥1.5)
-    scrolloff = 4, -- Set Glow’s scroll offset when using large files
-    filetypes = { "markdown", "md", "rmd" }, -- Filetypes that should trigger Glow by default (useful for auto-preview)
+    -- position = "right", -- Window position (center, top, bottom, left, right) only in flowting mod
+    -- padding = 0, -- Padding between window border and content
+    -- log_level = "error", -- values: "error", "warn", "info", "debug" Enable or disable Glow's command output logging (for debugging)
+    -- auto_close = true, -- Automatically close Glow preview when you leave the buffer
+    -- float = false, -- Open Glow in a floating window (true) or full Neovim buffer (false) Set this to false if you want tab-based or split-based previews
+    -- -- glow_path = nil, -- Custom path to your `glow` binary if it's not in $PATH
+    -- syntax = true, -- Enable syntax highlighting within the Glow preview (requires glow ≥1.5)
+    -- scrolloff = 4, -- Set Glow’s scroll offset when using large files
+    -- filetypes = { "markdown", "md", "rmd" }, -- Filetypes that should trigger Glow by default (useful for auto-preview)
   },
 }
