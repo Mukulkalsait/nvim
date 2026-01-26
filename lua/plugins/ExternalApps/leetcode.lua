7dfb12461252679b062967c2ea1ff22c19f7882e
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-01-26 20:42:52 +0530

    removedc from wrong place

--- lua/plugins/ExternalApps/leetcode.lua
+++ lua/plugins/ExternalApps/leetcode.lua
@@ -10,11 +10,9 @@ return {
   },
   config = function()
     -- Ensure the directory exists before setup
-    local home_path = "/home/mukuldk/1_file/3_Repos_All/A_gitHub/2_rust-Programming/3_leetCode/leetnvim/src/bin/"
-    
+    local home_path = "/home/mukuldk/1_file/3_Repos_All/A_gitHub/002_Rust_Programing/3_leetCode/leetnvim/src/bin/"
     -- Create directory if it doesn't exist
     vim.fn.mkdir(home_path, "p")
-    
     require("leetcode").setup({
       storage = {
         home = home_path,
