ee2b06485a5deed23ac04a2137dc749212201791
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-03-10 11:23:41 +0530

    Updates

--- lua/plugins/Utility/folatingTermSnacks.lua
+++ lua/plugins/Utility/folatingTermSnacks.lua
@@ -1,12 +1,35 @@
+-- return {
+--   {
+--     "folke/snacks.nvim",
+--     opts = {
+--       terminal = {
+--         shell = vim.o.shell, -- Y: or /etc/profiles/per-user/mukuldk/bin/zsh
+--         win = {
+--           position = "float",
+--         },
+--       },
+--     },
+--   },
+-- }
 return {
   {
     "folke/snacks.nvim",
     opts = {
       terminal = {
-        shell = vim.o.shell, -- Y: or /etc/profiles/per-user/mukuldk/bin/zsh
+        shell = vim.o.shell,
         win = {
           position = "float",
         },
+      },
+    },
+    keys = {
+      {
+        "<C-/>",
+        function()
+          require("snacks").terminal()
+        end,
+        mode = { "n", "t" },
+        desc = "Toggle Terminal",
       },
     },
   },
