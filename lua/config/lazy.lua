c394c260a15c83991767605490d4a0ba9be3522b
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-03-10 12:36:27 +0530

    Deletion and clearinng

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,23 +14,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- costume performance.lua config
-require("config.performance")
-
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
-    { import = "plugins/Lsp/" },
-    { import = "plugins/Lsp/webdev" },
-    -- { import = "plugins/Lsp/Rust" },
-    -- { import = "plugins/Lsp/Java" },
-    { import = "plugins/Themes" },
-    { import = "plugins/Utility" },
-    { import = "plugins/Colors" },
-    { import = "plugins/ExternalApps" },
+    { import = "plugins.utils"},
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
@@ -42,13 +32,9 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-
-  -- // R: ROCKS LUA luarocks are stoped here.
-  rocks = { enabled = false, },
-
   checker = {
     enabled = true, -- check for plugin updates periodically
-    notify = true, -- notify on update
+    notify = false, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
     rtp = {
@@ -60,27 +46,9 @@ require("lazy").setup({
         -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
+        "tutor",
         "zipPlugin",
-        --tabline B: using => tabline.lua
-        "akinsho/bufferline.nvim",
       },
     },
   },
-  -- {
-  --   "jose-elias-alvarez/null-ls.nvim",
-  --   requires = { "nvim-lua/plenary.nvim" },
-  -- },
 })
-
-
--- DX: Debuging
--- this code will show if HTML is slowly saving.
--- vim.api.nvim_create_autocmd("BufWritePre", {
---   pattern = "*.html",
---   callback = function()
---     print("Save starting")
---     vim.defer_fn(function()
---       print("Save completed")
---     end, 0)
---   end,
--- })
