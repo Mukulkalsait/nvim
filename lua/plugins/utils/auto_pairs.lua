e5df832f9f6d8ec00a4ac3a19becc0a0d54566e8
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-04-15 19:22:39 +0530

    Updates

---
+++ lua/plugins/utils/auto_pairs.lua
@@ -0,0 +1,19 @@
+return {
+  -- closing pairs inserting or {} / [] / ()
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {
+      enable_check_bracket_line = true,
+      check_ts = true,
+      fast_wrap = {
+        map = "<M-e>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = [=[[%'%"%>%]%)%}%,]]=],
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+      },
+    },
+  },
+}
