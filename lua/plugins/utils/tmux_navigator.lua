36a49b0f0c0a41c6f54082cdabee2fed6f78e829
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-02 02:52:48 +0530

    Tmux_navigator

---
+++ lua/plugins/utils/tmux_navigator.lua
@@ -0,0 +1,36 @@
+return
+
+-- {
+--  "christoomey/vim-tmux-navigator"
+--  }
+
+-- { 'alexghergh/nvim-tmux-navigation', config = function()
+--     local nvim_tmux_nav = require('nvim-tmux-navigation')
+--     nvim_tmux_nav.setup {
+--         disable_when_zoomed = true,
+--         -- keybindings = {
+--         --     left = "<C-h>",
+--         --     down = "<C-j>",
+--         --     up = "<C-k>",
+--         --     right = "<C-l>",
+--         --     last_active = "<C-\\>",
+--         --     next = "<C-Space>",
+--         -- }
+--     }
+-- end
+-- }
+
+{
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft", "TmuxNavigateDown",
+    "TmuxNavigateUp", "TmuxNavigateRight",
+  },
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+  },
+}
+
