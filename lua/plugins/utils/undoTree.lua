76684d1c50e7248b711615daabbb035e58780774
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-12 02:02:54 +0530

    undo tree vmode to <Space> R T

--- lua/plugins/utils/undoTree.lua
+++ lua/plugins/utils/undoTree.lua
@@ -1,19 +1,4 @@
--- Y: undo tree its super awsome.
--- return {
---   "jiaoshijie/undotree",
---   dependencies = "nvim-lua/plenary.nvim",
---   config = function()
---     require("undotree").setup({
---       window = {
---         width = 20, -- reduce width (default is 40)
---         -- height = 15, -- reduce height (default varies)
---       },
---     })
---   end,
---   keys = {
---     { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
---   },
--- }
+-- working Perfectly
 return{
   "jiaoshijie/undotree",
   opts = {
@@ -21,6 +6,6 @@ return{
   },
   keys = { -- load the plugin only when using it's keybinding:
     -- { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
-    { "<leader>tu", "<cmd>lua require('undotree').toggle()<cr>" , desc = "Undo Tree", },
+    { "<leader>tu", "<cmd>lua require('undotree').toggle()<cr>" , desc = "🌲 Undo Tree" },
   },
 }
