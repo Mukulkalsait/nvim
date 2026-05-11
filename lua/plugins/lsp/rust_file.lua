32e2a22211b233a30413d5752c8a0b01a1f9dbf7
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-12 02:03:52 +0530

    the rust_file

--- lua/plugins/lsp/rust_file.lua
+++ lua/plugins/lsp/rust_file.lua
@@ -1,7 +1,28 @@
+-- return {
+--   {
+--     "mrcjkb/rustaceanvim",
+--     version = "^4",
+--     ft = { "rust" },
+--   },
+-- }
+
 return {
   {
     "mrcjkb/rustaceanvim",
     version = "^4",
     ft = { "rust" },
+
+    config = function()
+      vim.g.rustaceanvim = {
+        tools = {
+          cargo = {
+            command = vim.fn.exepath("cargo"),
+          },
+        },
+        server = {
+          cmd = { vim.fn.exepath("rust-analyzer") },
+        },
+      }
+    end,
   },
 }
