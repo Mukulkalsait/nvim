c3c6c493bfa89f7ccc02e7fb3838725bbeb5b8d5
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-12 02:03:25 +0530

    Treesitter working

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,37 +1,44 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = false,
+    build = ":TSUpdate",
     opts = {
-      auto_install = false,
-
+      auto_install = true,
       ensure_installed = {
-        -- "markdown",
-        -- "markdown_inline",
-        -- "php",
-        -- "rust",
-        -- "python",
-        -- "javascript",
-        -- "typescript",
-        -- "lua",
-        -- "bash",
+        "markdown",
+        "markdown_inline",
+        "php",
+        "rust",
+        "python",
+        "javascript",
+        "typescript",
+        "lua",
+        "yaml",
+        "bash",
       }, -- Empty - install via Nix instead
 
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = { "markdown" },
+        -- additional_vim_regex_highlighting = false,
       },
 
       indent = { enable = true },
+      modules = {},
 
+      -- code_blocks = {
+      --   enable = true,
+      --   icons = true,
+      --   style = "minimal",
+      --   highlight = "treesitter",  -- if supported
+      -- },
       -- Better code block highlighting in markdown
-      injections = {
-        markdown = { enable = true },
-      },
+      -- injections = {
+      --   markdown = { enable = true },
+      -- },
     },
   },
 
-  -- Fixed autotag (loads AFTER Treesitter is ready)
+  -- Fixed autotag (loads AFTER Treesitter is ready) B: WORKING PERFECTLY...
   {
     "windwp/nvim-ts-autotag",
     event = "VeryLazy", -- Important: load later
@@ -45,3 +52,4 @@ return {
     end,
   },
 }
+
